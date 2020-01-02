2663d372f8655f207a7ecb6f54b3505dca46e7ac
devshankha <45039485+devshankha@users.noreply.github.com>
2020-01-02 21:20:31 +0530

    Update BitMasks.java

--- BitMasks/src/BitMasks.java
+++ BitMasks/src/BitMasks.java
@@ -22,6 +22,16 @@ public class BitMasks {
 		b = (byte) (b & ~(1 << n));
 
 	}
+	//find the number of set bits in a byte
+	static int setBits(int n) {
+		int c= 0;
+		while (n != 0) {
+			n = n&(n-1);
+			c++;
+		}
+		return c;
+		
+	}
 
 	static void toggleNthbit(byte b, int n) {
 
